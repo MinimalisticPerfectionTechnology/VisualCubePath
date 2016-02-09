1b64397bf58254c63edfad459d5f648bc10ff258
Solver <robin.jernestrand@gmail.com>
2016-02-09 22:23:46 +0100

    adding shader

--- VisualCubePath/src/se/MPT/engineTester/MainGameLoop.java
+++ VisualCubePath/src/se/MPT/engineTester/MainGameLoop.java
@@ -1,3 +1,4 @@
+
 package se.MPT.engineTester;
 
 import org.lwjgl.opengl.Display;
@@ -6,6 +7,7 @@ import se.MPT.renderEngine.DisplayManager;
 import se.MPT.renderEngine.Loader;
 import se.MPT.renderEngine.RawModel;
 import se.MPT.renderEngine.Renderer;
+import se.MPT.shaders.StaticShader;
 
 public class MainGameLoop {
 	public static void main(String[] args) {
@@ -13,27 +15,31 @@ public class MainGameLoop {
 
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 
 		float[] vertices = { 
-				-0.5f, 0.5f, 0,	//V0
-				-0.5f, -0.5f, 0, //V1
-				0.5f, -0.5f, 0, //V2
-				-0.5f, 0.5f, 0 	//V3
-				};
+				-0.5f, 0.5f, 0, 	// V0
+				-0.5f, -0.5f, 0, 	// V1
+				0.5f, -0.5f, 0, 	// V2
+				0.5f, 0.5f, 0 		// V3
+		};
 
 		int[] indices = { 
-				0, 1, 3, // Top left triangle
-				3, 1, 2// Bottom right triangle
+				0, 1, 3, 	// Top left triangle
+				3, 1, 2		// Bottom right triangle
 		};
 
 		RawModel model = loader.loadToVAO(vertices, indices);
 
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
---
+++ VisualCubePath/src/se/MPT/shaders/StaticShader.java
@@ -0,0 +1,17 @@
+package se.MPT.shaders;
+
+public class StaticShader extends ShaderProgram {
+
+	private static final String VERTEX_FILE = "src/se/MPT/shaders/vertexShader.txt";
+	private static final String FRAGMENT_FILE = "src/se/MPT/shaders/fragmentShader.txt";
+
+	public StaticShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+
+	@Override
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+	}
+
+}
